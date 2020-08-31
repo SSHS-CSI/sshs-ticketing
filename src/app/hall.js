4dcaec64cd71619e91b6ef5cb3cebf05a5de575c
Sungbin Jo <pcr910303@icloud.com>
2020-08-31 19:17:09 +0900

    Add taken state to Seat component

--- src/app/hall.js
+++ src/app/hall.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import { Button } from "@blueprintjs/core";
 
-function Seat({ column = 0, row = 0, disabled = false }) {
+function Seat({ column = 0, row = 0, disabled = false, taken = false }) {
     let title = `${String.fromCharCode(column + 65)}${row}`;
     return (
         <td>
             <Button
                 style={{ width: "100%" }}
-                disabled={disabled}
-                outlined
-                text={title}
+                disabled={disabled || taken}
+                outlined={!taken}
+                intent={taken ? "primary" : "none"}
+                icon={taken ? "cross" : undefined}
+                text={!taken ? title : undefined}
                 onClick={() => alert(title)} />
         </td>
     );
@@ -19,23 +21,24 @@ function Seat({ column = 0, row = 0, disabled = false }) {
 export default function Hall() {
     return (
         <div style={{ overflowX: "auto" }}>
-            <table>
+            <table style={{ margin: "16px auto" }}>
                 {Array.from(new Array(10).keys()).map(i => (
                     <tr>
                         {Array.from(new Array(7).keys()).map((j) => (
                             <Seat
                                 column={i}
                                 row={j}
-                                disabled={(i == 2 && j == 3) || (i == 1 && j == 3) || (i == 3 && j == 3) || (i == 2 && j == 2) || (i == 2 && j == 4)}
+                                disabled={(i + j) % 2 == 0}
+                                taken={(i == 3 && j == 2)}
                             />
                         ))}
                         <td style={{ width: 8 }} />
                         {Array.from(new Array(10).keys()).map((j) => (
-                            <Seat column={i} row={j + 7} />
+                            <Seat column={i} row={j + 7} disabled={(i + j) % 2 == 1} />
                         ))}
                         <td style={{ width: 8 }} />
                         {Array.from(new Array(7).keys()).map((j) => (
-                            <Seat column={i} row={j + 17} />
+                            <Seat column={i} row={j + 17} disabled={(i + j) % 2 == 1} />
                         ))}
                     </tr>
                 ))}
