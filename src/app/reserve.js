d61cdd7bdcc7c203f3eb25b5a6d6a289f88e674f
Sungbin Jo <pcr910303@icloud.com>
2020-09-01 10:00:40 +0900

    Add loading, succeeded and failed states to Reserve component

--- src/app/reserve.js
+++ src/app/reserve.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FormGroup, ControlGroup, InputGroup, Button } from "@blueprintjs/core";
 
 export default function Reserve() {
+    let [state, setState] = useState("none");
+    let intent, loading, icon;
+    switch (state) {
+    case "none":
+        intent = "none";
+        loading = false;
+        icon = "arrow-right";
+        break;
+    case "loading":
+        intent = "none";
+        loading = true;
+        icon = undefined;
+        break;
+    case "succeeded":
+        intent = "success";
+        loading = false;
+        icon = "tick";
+        break;
+    case "failed":
+        intent = "danger";
+        loading = false;
+        icon = "error";
+        break;
+    }
+
+    function toggleState() {
+        setState(state => {
+            switch (state) {
+            case "none":
+                setTimeout(() => setState("succeeded"), 1000);
+                return "loading";
+            case "loading":
+                return "loading";
+            case "succeeded":
+                return "none";
+            case "failed":
+                return "none";
+            }
+            return state;
+        });
+    }
+
     return (
-        <FormGroup style={{ maxWidth: "80%", width: 640, margin: "auto" }} label="School ID" labelInfo="(required)">
+        <FormGroup style={{ maxWidth: "80%", width: 640, margin: "auto" }} label="Ticketing Information">
             <ControlGroup fill>
-                <InputGroup placeholder="18113" />
-                <Button className="bp3-fixed" icon="arrow-right" />
+                <InputGroup placeholder="School ID" />
+                <InputGroup placeholder="Keycode" />
+                <Button
+                    className="bp3-fixed"
+                    intent={intent}
+                    icon={icon}
+                    loading={loading}
+                    onClick={() => toggleState()} />
             </ControlGroup>
         </FormGroup>
     );
